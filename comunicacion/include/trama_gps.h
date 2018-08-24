c9cfa1ba633764ef49ac62fbc83d6934ee339aee
manueli <m.iglesias91@gmail.com>
2018-08-24 16:16:06 -0300

    parseo de trama gps

--- comunicacion/include/trama_gps.h
+++ comunicacion/include/trama_gps.h
@@ -3,6 +3,9 @@
 // stl
 #include <string>
 
+// herramientas
+#include <utiles/include/Fecha.h>
+
 // aplicacion
 #include <aplicacion/include/configuracion.h>
 
@@ -13,6 +16,11 @@ namespace seon::comunicacion {
 
 class trama_gps : public trama {
 
+    struct coordenada {
+        double_t angulo;
+        std::string cardinalidad;
+    };
+
 public:
 
     trama_gps();
@@ -21,7 +29,13 @@ public:
 
     virtual bool setear(const std::string & tira_de_datos);
 
+    std::string id, status, checksum;
+    coordenada latitude, longitude, magnetic_variation;
+    double_t velocity, angle;
+    herramientas::utiles::Fecha date;
+
 private:
+    bool parsear(const std::string & tira_de_datos);
 
     static seon::aplicacion::configuracion::trama configuracion;
 };
--- comunicacion/source/trama_gps.cpp
+++ comunicacion/source/trama_gps.cpp
@@ -1,6 +1,7 @@
 #include <comunicacion/include/trama_gps.h>
 
 // herramientas
+#include <utiles/include/FuncionesString.h>
 #include <utiles/include/ImposibleAbrirArchivo.h>
 
 // aplicacion
@@ -21,6 +22,29 @@ trama_gps::~trama_gps() {
 bool trama_gps::setear(const std::string & tira_de_datos) {
 
     this->trama::setear(tira_de_datos);
+
+    return true;
+}
+
+bool trama_gps::parsear(const std::string & tira_de_datos) {
+    std::vector<std::string> campos = herramientas::utiles::FuncionesString::separar(tira_de_datos, ",");
+
+    if (campos.size() != 13) {
+        return false;
+    }
+
+    this->id = campos[0];
+    herramientas::utiles::Fecha::parsear(campos[9] + campos[1], "%d%m%y%H%M%S", &this->date);
+    this->status = campos[2];
+    this->latitude.angulo = std::stod(campos[3]);
+    this->latitude.cardinalidad = campos[4];
+    this->longitude.angulo = std::stod(campos[5]);
+    this->longitude.cardinalidad = campos[6];
+    this->velocity = std::stod(campos[7]);
+    this->angle = std::stod(campos[8]);
+    this->magnetic_variation.angulo = std::stod(campos[10]);
+    this->magnetic_variation.cardinalidad = campos[11];
+    this->checksum = campos[12];
 
     return true;
 }
